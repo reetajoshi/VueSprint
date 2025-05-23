b3130145809e9df863416cdd3af40a752faf7b91
K SAI SRAVANI <sravanik0105@gmail.com>
2025-05-23 19:38:24 +0530

    Intial commit day1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Welcome to Vuejs  More Excited</h1>
+</template>
+
+<script setup>
+// No need for code here for now
+</script>
+
+<style scoped>
+h1 {
+  color:rgb(192, 227, 17);
+  font-family: 'Arial', sans-serif;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
