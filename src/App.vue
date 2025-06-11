d2a22c90e567d2a7893b1f1ee932a012411cfa95
sreedevi311 <venupulaparthi1968@gmail.com>
2025-06-11 11:37:16 +0530

    Added date 21 class code

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <template>
-  <DefaultLayout>
-    <router-view />
-  </DefaultLayout>
+    <div class="container">
+        <div class="bg-info p-5 m-3 rounded text-center">
+            <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+            <h1>{{ message }}</h1>
+        </div>
+    </div>
 </template>
-<script setup>
-import DefaultLayout from '@/layouts/Default.vue'
+
+<script>
+    export default{
+        data(){
+            return{
+                message:"Hello, World!"
+            }
+        }
+    }
 </script>
+
+<style>
+    h1{
+        color:#35495e;
+        margin-left: 20px;
+    }
+    .bg-info{
+        background-color: rgb(2, 214, 143) !important;
+    }
+    
+</style>
