3485e789117bf1112dadf1dc4a58018ac4bca273
sreedevi311 <venupulaparthi1968@gmail.com>
2025-06-13 11:47:33 +0530

    Added date 23 class code

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,32 @@
 <template>
-    <div class="container">
-        <div class="bg-info p-5 m-3 rounded text-center">
-            <h1>{{ msg }}</h1>
-            <img alt="Vue logo" class="logo" :src="url" width="125" height="110" />
-            <p v-text="text"></p>
-            <div v-html="html"></div>
-            <div><button @click="sayHello" class="btn btn-primary p-2 m-2 rounded" v-if="showButton">Click Me!</button>
-            <p v-else>Button Hidden!</p></div>
-            <p v-for="word in arr" style="display: inline-block;">{{ word }}</p>
-        </div>
-    </div>
+        <props 
+        msg="This is a message passed by props"
+        :arr="itemsArr"
+        @greet="handleGreet"></props>
 </template>
 
 <script setup>
+    import props from './components/propsAndEmits(3).vue';
     import {ref} from 'vue'
-    const msg=ref("Hello, Vue!")
-    const url=ref("https://cdn.freebiesupply.com/logos/large/2x/vue-9-logo-png-transparent.png")
-    const text=ref("This is text using v-text")
-    const html=ref("<h3>This is h3 tag using v-html</h3>")
-    const showButton=ref(true)
-    const sayHello=()=>{
-        alert('Hey! Hello')
-        showButton.value=false
+    const itemsArr=ref([
+        {id:1,word:'Each '},
+        {id:2,word:'word '},
+        {id:3,word:'in '},
+        {id:4,word:'this '},
+        {id:5,word:'para '},
+        {id:6,word:'is '},
+        {id:7,word:'from '},
+        {id:8,word:'an '},
+        {id:9,word:'array '},
+        {id:10,word:'passed '},
+        {id:11,word:'through'},
+        {id:12,word:'props'}])
 
+    const handleGreet=(msg)=>{
+        alert(msg)
     }
-    const arr=ref(['The','words','in','this','para','are','displayed','using','v-for.'])
 </script>
 
-<style>
-    body{
-        background-color: #242424;
-    }
-    h1,h3,p{
-        color:#35495e;
-        margin-left: 20px;
-    }
-    .bg-info{
-        background-color: rgb(2, 214, 143) !important;
-    }
-    .btn-primary{
-        background-color: #35495e !important;
-        border: none;
-    }
-    
+<style scoped>
+
 </style>
---
+++ src/components/HelloWorld(1).vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="bg-info p-5 m-3 rounded text-center">
+            <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+            <h1>{{ message }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                message:"Hello, World!"
+            }
+        }
+    }
+</script>
+
+<style>
+    h1{
+        color:#35495e;
+        margin-left: 20px;
+    }
+    .bg-info{
+        background-color: rgb(2, 214, 143) !important;
+    }
+    
+</style>
