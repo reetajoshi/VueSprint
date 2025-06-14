fc265d885348683d3c479ef0088097ea14443a00
sreedevi311 <venupulaparthi1968@gmail.com>
2025-06-14 17:18:50 +0530

    Added date 24 class code

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
-        <props 
-        msg="This is a message passed by props"
-        :arr="itemsArr"
-        @greet="handleGreet"></props>
+        <div class="container p-5">
+            <lifecycleHooks/>
+            <div class="bg-info p-2 m-4 rounded text-center"> |
+                <router-link to="/">Home</router-link> |
+                <router-link :to="{ name: 'about' }">About</router-link> |
+                <router-link :to="`/profile/101`">Profile 101</router-link> |
+                <router-view></router-view>
+            </div>
+        </div>
 </template>
 
 <script setup>
-    import props from './components/propsAndEmits(3).vue';
-    import {ref} from 'vue'
-    const itemsArr=ref([
-        {id:1,word:'Each '},
-        {id:2,word:'word '},
-        {id:3,word:'in '},
-        {id:4,word:'this '},
-        {id:5,word:'para '},
-        {id:6,word:'is '},
-        {id:7,word:'from '},
-        {id:8,word:'an '},
-        {id:9,word:'array '},
-        {id:10,word:'passed '},
-        {id:11,word:'through'},
-        {id:12,word:'props'}])
-
-    const handleGreet=(msg)=>{
-        alert(msg)
-    }
+    import lifecycleHooks from './components/lifecycleHooks(4).vue'
 </script>
 
-<style scoped>
-
+<style>
+    body{
+        background-color: #242424;
+        color:black;
+    }
+    .bg-info{
+        background-color:rgb(3, 233, 156) !important;
+    }
 </style>
---
+++ src/components/lifecycleHooks(4).vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <div class="bg-info p-2 m-3 rounded text-center">
+            <h2>Vue3 App</h2>
+            <img alt="Vue logo" class="logo p-2 m-2" src="https://cdn.freebiesupply.com/logos/large/2x/vue-9-logo-png-transparent.png" width="125" height="125" />
+            <h4>count: {{ counter }}</h4>
+            <button class="btn btn-primary p-2 m-2" @click="increment">Increment</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import {onMounted, ref,onUnmounted, onBeforeUpdate, onUpdated} from 'vue'
+    let counter=ref(0)
+    const increment=()=>{
+        counter.value++
+    }
+
+    onMounted(()=>{
+        console.log("componenet mounted")
+    })
+
+    onBeforeUpdate(()=>{
+        console.log("on before update")
+    })
+
+    onUpdated(()=>{
+        console.log("on update")
+    })
+
+
+    onUnmounted(()=>{
+        console.log("componenet unmounted")
+    })
+</script>
+
+<style scoped>
+    h2,h4{
+        color:#35495e;
+    }
+    .btn-primary{
+        background-color: #35495e !important;
+        color:white !important;
+        border: none;
+    }
+    .bg-info{
+        background-color: rgb(2, 214, 143) !important;
+    }
+</style>
